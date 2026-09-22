5ddfd62b3108b39e86f7ea0762fa50e58bb2baa5
nifets <stefanmanol@gmail.com>
2026-09-22 21:11:11 +0100

    make hybridtau delegate tdepgraph trait to exact method

--- src/aggregators/aggregators.jl
+++ src/aggregators/aggregators.jl
@@ -234,7 +234,7 @@ needs_depgraph(aggregator::CCNRM) = true
 needs_depgraph(aggregator::RDirect) = true
 needs_depgraph(aggregator::Coevolve) = true
 needs_depgraph(aggregator::TauSplitting) = true
-needs_depgraph(aggregator::HybridTau) = true
+needs_depgraph(aggregator::HybridTau) = needs_depgraph(aggregator.exact)
 
 # true if aggregator requires a map from solution variable to dependent jumps.
 # It is implicitly assumed these aggregators also require the reverse map, from
